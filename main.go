83c1fd8d8fc7e0497047740c576a97e5fbb59cc4
lindy2076 <kakaporuqee@gmail.com>
2024-04-27 23:41:34 +0300

    context added

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
 )
 
 var frames = []rune{'-', '\\', '|', '/'}
-var frames2 = []rune{'R', 'A', 'C', 'E'}
+var frames2 = []rune{'0', '1', '2', '3', '4', '5'}
 
 type LoadingAnimation struct {
 	frames  []rune
@@ -38,14 +39,27 @@ func (l *LoadingAnimation) NextFrameReverse() rune {
 }
 
 // Prints next frame and then blocks a thread for 1000/fps millis.
-func (l *LoadingAnimation) PrintNextFrame() {
+func (l *LoadingAnimation) PrintNextFrame(ctx context.Context) {
 	if !l.reverse {
 		fmt.Printf("%c", l.NextFrame())
 	} else {
 		fmt.Printf("%c", l.NextFrameReverse())
 	}
-	<-l.ticker.C
 	fmt.Printf("\b")
+	select {
+	case <-ctx.Done():
+	case <-l.ticker.C:
+	}
+}
+
+// Plays animation until the context is cancelled.
+func (l *LoadingAnimation) Play(ctx context.Context) {
+	for {
+		if ctx.Err() != nil {
+			return
+		}
+		l.PrintNextFrame(ctx)
+	}
 }
 
 // Creates new LoadingAnimation. Frames is slice of runes that will be displayed in
@@ -62,45 +76,39 @@ func NewLoadingAnimation(frames []rune, fps int, reverse bool) *LoadingAnimation
 func main() {
 	fmt.Println("WHAT A WONDERFUL LOADING ANIMATION")
 
+	mainCtx := context.Background()
+
+	ctx, cancel := context.WithCancelCause(mainCtx)
 	// some time consuming job
 	var wg sync.WaitGroup
 	wg.Add(1)
-	jobDone := make(chan bool)
-	go func(jobDone chan<- bool) {
-		defer func() {
-			wg.Done()
-			jobDone <- true
-		}()
-
-		time.Sleep(time.Second * 3)
-	}(jobDone)
-
+	go func() {
+		defer wg.Done()
+		time.Sleep(2 * time.Second)
+		cancel(fmt.Errorf("job done"))
+	}()
 	// one way 5fps animation
 	loadingAnimation := NewLoadingAnimation(frames, 5, false)
 	fmt.Printf("LOADING:")
-	go func(jobDone <-chan bool) {
-		for {
-			select {
-			case <-jobDone:
-				return
-			default:
-				loadingAnimation.PrintNextFrame()
-			}
-		}
-	}(jobDone)
+	go loadingAnimation.Play(ctx)
 
 	wg.Wait()
-	fmt.Println("\nDONE")
+	fmt.Printf("%c\n", '✓')
 
 	fmt.Println("AND ANOTHER ONE")
-	// 3fps animation with reverse
-	loadingAnimation2 := NewLoadingAnimation(frames2, 3, true)
-	fmt.Printf("LOADING:")
+
+	ctx, cancel = context.WithCancelCause(mainCtx)
+	wg.Add(1)
 	go func() {
-		for {
-			loadingAnimation2.PrintNextFrame()
-		}
+		defer wg.Done()
+		time.Sleep(3 * time.Second)
+		cancel(fmt.Errorf("job done"))
 	}()
-	time.Sleep(5 * time.Second)
-	fmt.Println("\nDONE")
+	// reverse 8fps animation
+	loadingAnimation2 := NewLoadingAnimation(frames2, 8, true)
+	fmt.Printf("PROCESSING:")
+	go loadingAnimation2.Play(ctx)
+
+	wg.Wait()
+	fmt.Printf("%c\n", '✓')
 }
